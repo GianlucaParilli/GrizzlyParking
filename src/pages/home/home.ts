b39aa62b0bbf9729bb95c998e11a39a1a330e82d
jtran6 <jtran6@ggc.edu>
2018-02-13 02:16:42 -0500

    got home page working again. working on H Lot page now

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { HLotPage } from './../h-lot/h-lot';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToHLotPage() {
+    this.navCtrl.push(HLotPage);
   }
 
 }

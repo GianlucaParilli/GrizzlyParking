eea48e9ce5b2ac61354ec7e8ef2c8bbe3f522b51
chelCcat <chocoboangel@gmail.com>
2018-02-18 18:28:52 -0500

    added angularfirebase2 setup in src/app.module.ts & package.json.  Also staged beginnings of firebase usage in the map.ts and h-lot.ts files, including functions to be called from the pages usages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,56 @@
-import { StatisticsPage } from './../pages/statistics/statistics';
-import { HLotPage } from './../pages/h-lot/h-lot';
+// IMPORTS
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { StatusBar } from '@ionic-native/status-bar';
+// SPLASHSCREEN
 import { SplashScreen } from '@ionic-native/splash-screen';
+// PAGES
+import { HomePage } from '../pages/home/home';
 import { MapPage } from '../pages/map/map';
+import { HLotPage } from './../pages/h-lot/h-lot';
+import { StatisticsPage } from './../pages/statistics/statistics';
+// tabs
+import { TabsPage } from '../pages/tabs/tabs';
+// OTHER
+import { StatusBar } from '@ionic-native/status-bar';
+// ANGULAR FIREBASE
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+// SETTINGS - ANGULAR FIREBASE
+export const firebaseConfig = {
+  apiKey: "AIzaSyCxKh1mXmfd2o76dslPZFzLyJNdY5LPRMo",
+  authDomain: "grizzlyparking-1111a.firebaseapp.com",
+  databaseURL: "https://grizzlyparking-1111a.firebaseio.com",
+  projectId: "grizzlyparking-1111a",
+  storageBucket: "grizzlyparking-1111a.appspot.com",
+  messagingSenderId: "251820964455"
+};
+
+// SETTINGS - NG MODULES
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     TabsPage,
-    HLotPage,
     MapPage,
+    HLotPage,
     StatisticsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     TabsPage,
-    HLotPage,
     MapPage,
+    HLotPage,
     StatisticsPage
   ],
   providers: [
--- src/pages/h-lot/h-lot.ts
+++ src/pages/h-lot/h-lot.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, AlertController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 /**
  * Generated class for the HLotPage page.
@@ -8,6 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+// FIREBASE ITEM
+// H-LOT has 2 lot areas, h1 and h2.
+lotArea: AngularFireList<any>;
+
 @IonicPage()
 @Component({
   selector: 'page-h-lot',
@@ -15,11 +20,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HLotPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public alertCtrl: AlertController,
+    afDatabase: AngularFireDatabase) {
+      //the list of lotAreas...
+      this.lotArea = afDatabase.list('/lotArea').valueChanges();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HLotPage');
+  }
+
+  parkedConfirmation(lot){ //int googleMapsLocation){
+    let prompt = this.alertCtrl.create({
+      title: 'Parked Vehicle Confirmation',
+      message: "Have you parked?",
+      buttons: [
+        {
+          text: 'No',
+          handler: data => {
+            console.log('~~~~~ BUTTON CLICKED: CAR NOT PARKED (NO)');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: data => {
+            console.log('~~~~~ BUTTON CLICKED: CAR PARKED (YES)');
+            this.updateLot(lot, 1);
+          }
+        }
+      ]
+    })
+  }
+
+  updateLot(lot: string, update: number){
+    // do a toast... lookup how to do that
+    // update db (add or subtract as needed)
+    handler: data => {
+      this.lotArea.update(lot, XXX
+    });
   }
 
 }

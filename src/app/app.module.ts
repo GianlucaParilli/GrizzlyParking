110905b569ae3a1b6b2ad4e7e95a81bb5bae388d
chelCcat <chocoboangel@gmail.com>
2018-02-19 18:27:16 -0500

    Changed firebase account setup info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,14 +33,15 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 // SETTINGS - ANGULAR FIREBASE
 export const firebaseConfig = {
-  apiKey: "AIzaSyCxKh1mXmfd2o76dslPZFzLyJNdY5LPRMo",
-  authDomain: "grizzlyparking-1111a.firebaseapp.com",
-  databaseURL: "https://grizzlyparking-1111a.firebaseio.com",
-  projectId: "grizzlyparking-1111a",
-  storageBucket: "grizzlyparking-1111a.appspot.com",
-  messagingSenderId: "251820964455"
+  apiKey: "AIzaSyBMri8zJn3iCZDD5iwklvwatW9nQlM0BnI",
+  authDomain: "grizzlyparking-2222a.firebaseapp.com",
+  databaseURL: "https://grizzlyparking-2222a.firebaseio.com",
+  projectId: "grizzlyparking-2222a",
+  storageBucket: "",
+  messagingSenderId: "375005973853"
 };
 >>>>>>> added angularfirebase2 setup in src/app.module.ts & package.json.  Also staged beginnings of firebase usage in the map.ts and h-lot.ts files, including functions to be called from the pages usages
+
 
 // SETTINGS - NG MODULES
 @NgModule({

b9f37eba9589a29522c20101814d93a8ad023f83
chelCcat <chocoboangel@gmail.com>
2018-02-26 22:45:27 -0500

    Added several functions to handle firebase data. Add/delete location. Deal with aggregate data (population, percent), lotArea assignment and add/remove user from its list of users

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { HLotPage } from './../h-lot/h-lot';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
@@ -59,42 +59,35 @@ export class HomePage {
     public afAuth: AngularFireAuth, 
     public afs:    AngularFirestore) {
 
-<<<<<<< HEAD
       // FIREBASE CONNECTION TO COLLECTIONS
-      this.afAuth.auth.signInAnonymously();
+      //this.afAuth.auth.signInAnonymously();
       this.userCollectionRef       = this.afs.collection('user');
       this.locationCollectionRef   = this.afs.collection('location'); 
       this.lotAreaCollectionRef    = this.afs.collection('lotArea'); 
       this.parkingLotCollectionRef = this.afs.collection('parkingLot');
 
       // USED IN HTML - listener?
-=======
-      //this.afAuth.auth.signInAnonymously();
-      this.userCollectionRef = this.afs.collection('user'); 
->>>>>>> f738244ed20508e730a37a16c022e2f4bd0faba5
       this.users = this.userCollectionRef.valueChanges();
       this.locations = this.locationCollectionRef.valueChanges();
       this.lotAreas = this.lotAreaCollectionRef.valueChanges();
       this.parkingLots = this.parkingLotCollectionRef.valueChanges();
   }
-  //Navigation
+
+
+  // PAGE NAVIGATION  |  H-Lot  -->  Login (returns after closing)
   goToHLotPage() {
     this.navCtrl.push(HLotPage);
   }
 
-<<<<<<< HEAD
-  // USER FUNCTIONS
-=======
   goToLoginPage(){
     this.navCtrl.push(LoginPage);
   }
 
 
->>>>>>> f738244ed20508e730a37a16c022e2f4bd0faba5
+  // COLLECTION FUNCTIONS  |  USER
   userDocRefID: string;
 
-  createUser(isParked: boolean, parkedLatitude: number,
-    parkedLongitude: number) {
+  createUser( isParked: boolean, parkedLatitude: number, parkedLongitude: number ) {
     console.log("~~~~~~~~~~ FUNCTION createUser called ", this.userDocRefID);
     
     this.userCollectionRef.add({
@@ -112,7 +105,7 @@ export class HomePage {
     });
   }
 
-  parkUser(user: UserInterface, latitude: number, longitude: number) {
+  parkUser( user: UserInterface, latitude: number, longitude: number, determinedLotArea: string ) {
     console.log("~~~~~~~~~~ FUNCTION parkUser called ", this.userDocRefID);
 
     this.userCollectionRef.doc(this.userDocRefID).update({ 
@@ -124,15 +117,17 @@ export class HomePage {
     .catch(function(error) {
       console.log("~~~~~~~~~~ FUNCTION parkUser error ", error);
     });
+
+    this.assignLotArea( determinedLotArea );
   }
 
-  unparkUser(user: UserInterface) {
+  unparkUser( user: UserInterface ) {
     console.log("~~~~~~~~~~ FUNCTION unparkUser called ", this.userDocRefID);
 
     this.userCollectionRef.doc(this.userDocRefID).update({ 
       isParked: true, 
-      parkedLatitude: 33.980466,
-      parkedLongitude: -84.002062,
+      parkedLatitude: 33.0,
+      parkedLongitude: -84.0,
       parkedTime: timestamp
     })
     .catch(function(error) {
@@ -140,5 +135,100 @@ export class HomePage {
     });
   }
 
+  deleteUser( user: UserInterface ) {
+    console.log("~~~~~~~~~~ FUNCTION deleteUser called");
+
+    this.userCollectionRef.doc(this.userDocRefID).delete()
+    .then(function() {
+      console.log("~~~~~~~~~~ FUNCTION deleteUser successful");
+      this.userDocRefID = "";
+    })
+    .catch(function(error) {
+      console.log("~~~~~~~~~~ FUNCTION deleteUser error ", error);
+    });
+  }
+
+
+  // COLLECTION FUNCTIONS  |  LOCATION
+  locationDocRefID: string;
+
+  createLocation( latitude: number, longitude: number ) {
+    console.log("~~~~~~~~~~ FUNCTION createLocation called ", this.locationDocRefID);
+    
+    this.locationCollectionRef.add({
+      latitude: latitude,
+      longitude: longitude
+    })
+    .then(function(docRef) {
+      this.locationDocRefID = docRef.id;
+      console.log("~~~~~~~~~~ FUNCTION createLocation successful - ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("~~~~~~~~~~ FUNCTION createLocation error ", error);
+    });
+  }
+
+  deleteLocation( location: LocationInterface) {
+    console.log("~~~~~~~~~~ FUNCTION deleteLocation called");
+
+    this.locationCollectionRef.doc(this.locationDocRefID).delete()
+    .then(function() {
+      console.log("~~~~~~~~~~ FUNCTION deleteLocation successful");
+      this.locationDocRefID = "";
+    })
+    .catch(function(error) {
+      console.log("~~~~~~~~~~ FUNCTION deleteLocation error ", error);
+    });
+  }
+
+
+  // COLLECTION FUNCTIONS  |  LOTAREA
+  lotAreaDocRefID: string; // h1 or h2 | for testing, use h1
+
+
+  assignLotArea( determinedLotArea: string ) {
+    this.lotAreaDocRefID = "h1"; //for testing, we need a function to determine map logic
+    //this.lotAreaDocRefID = "h2";
+  }
+
+  addUserFromLotArea( user: UserInterface ) {
+    console.log("~~~~~~~~~~ FUNCTION addUserFromLotArea called");
+
+    this.lotAreaCollectionRef.doc(this.lotAreaDocRefID).collection('users').doc(this.userDocRefID).delete()
+    .then(function() {
+      console.log("~~~~~~~~~~ FUNCTION addUserFromLotArea successful");
+
+      var parkingLotRef = this.parkingLotCollectionRef.doc("hLot");
+      // In a transaction, add the new rating and update the aggregate totals
+      // Compute new number of ratings
+      var newParkingLotPopulation = parkingLotRef.data().parkingLotPopulation + 1;
+      var newParkingLotCapacity = parkingLotRef.data().parkingLotCapacityn + 1;
+
+      // Compute new percent available
+      var newPercent = 100 - (( parkingLotRef.data().parkingLotPopulation / parkingLotRef.data().parkingLotCapacity) * 100);
+
+      // Commit to Firestore
+      parkingLotRef.update({
+          parkingLotPopulation: newParkingLotPopulation,
+          percentAvailable: newPercent
+      })
+      .catch(function(error) {
+        console.log("~~~~~~~~~~ FUNCTION addUserFromLotArea - update parkingLot numbers error ", error);
+      });
+
+    })
+    .catch(function(error) {
+      console.log("~~~~~~~~~~ FUNCTION addUserFromLotArea error ", error);
+    });
+  }
+
+  removeUserFromLotArea( user: UserInterface ) {
+    console.log("~~~~~~~~~~ FUNCTION removeUserFromLotArea called");
+
+    this.lotAreaCollectionRef.doc(this.lotAreaDocRefID).collection('users').doc(this.userDocRefID).delete()
+    .catch(function(error) {
+      console.log("~~~~~~~~~~ FUNCTION removeUserFromLotArea error ", error);
+    });
+  }
   
 }

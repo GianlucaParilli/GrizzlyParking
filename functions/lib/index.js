53d082ab8dbee6ec084b6fbd8cc7bd19dcfdb9f9
jtran6 <jtran6@ggc.edu>
2018-03-19 19:41:28 -0400

    created hellowrold function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
 //# sourceMappingURL=index.js.map
